c66e6127f4a3f9457558bc85b54cd0acc37c3958
Slavik <n3tw0rk.spy@gmail.com>
2015-07-02 23:25:50 +0300

    removed api keys

--- src/PGateway/Config.php
+++ src/PGateway/Config.php
@@ -6,13 +6,13 @@ class Config
 {
     public static $paymentSystems = [
         'PayPal' => [
-            'client_id' => 'AQ_6G__IJz0nUsFCFwHTmHyCbifWfygjYrc3DsUeNvmnXAX4elBjLjXps-PJBCxtP7hpOW2ijwMCewnJ',
-            'client_secret' => 'EAufx_FAjlA44SBmbPWMaoar3d2KoiYdOLSZNaeNGa24NVTr3btVAu9Y04NdZCZvbc0lr5WGVJPi8B7d'
+            'client_id' => '',
+            'client_secret' => ''
         ],
         'BrainTree' => [
-            'merchantId' => '9rq4ybnpy5gbhffb',
-            'publicKey' => '9qfk73h8gcmtdm2f',
-            'privateKey' => 'bc65bb60a90a1aa4f116e97b023fca53'
+            'merchantId' => '',
+            'publicKey' => '',
+            'privateKey' => ''
         ]
     ];
 
